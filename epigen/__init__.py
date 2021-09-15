68d2ac47d6bc9ac922bf7886c5b016094b00c619
Fabio Mazza <fab4mazz@gmail.com>
2021-09-15 15:36:08 +0200

    Fix imports

--- epigen/__init__.py
+++ epigen/__init__.py
@@ -1,3 +1,3 @@
 from .base import EpInstance
 
-from .epidemy_gen import epidemy_gen, epidemy_gen_epinstance, epidemy_gen_graph, epidemy_gen_new
+from .epidemy_gen import epidemy_gen_epinstance, epidemy_gen_graph, epidemy_gen_new
