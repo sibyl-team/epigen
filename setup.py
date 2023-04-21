07396a2064310833bd22fffd6568e34c0c82da11
Fabio Mazza <fab4mazz@gmail.com>
2023-04-21 12:17:43 +0200

    Push version code

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Sibyl-EpiGen",
-    version="0.1",
+    version="0.2",
     author="sibyl-team",
     packages=find_packages(),
     description="A package to generate synthetic epidemies on graphs",
